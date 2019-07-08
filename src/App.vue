4c2bc2e7e4a65d0687315b3b68c80821f90f59c2
fengwei <36244881@qq.com>
2019-07-08 11:30:57 +0800

    测试vscode提交

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <!-- <router-link to="/">Home</router-link> | -->
-      <router-link to="/about">About</router-link>|
+      <router-link to="/about">About</router-link>|wdwdwdw
       <router-link to="/login">login</router-link>|
       <router-link to="/about1">about1</router-link>
     </div>
